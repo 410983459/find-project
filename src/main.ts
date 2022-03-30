ed0563799a0d9d0f59102712c651fac30660c44c
zhoucong <410983459@qq.com>
2022-03-30 10:47:19 +0800

    style：样式调整

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@
  * @Author: ZhouCong
  * @Date: 2022-02-24 13:56:03
  * @LastEditors: ZhouCong
- * @LastEditTime: 2022-03-03 17:56:42
+ * @LastEditTime: 2022-03-30 10:21:43
  * @Description: file content
  * @FilePath: \find-project\src\main.ts
  */
@@ -13,6 +13,7 @@ import store from './store'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 import './assets/css/reset.css'
+import '@/theme/commStyle.css'
 // 富文本
 import VueMarkdownEditor from '@kangc/v-md-editor';
 import '@kangc/v-md-editor/lib/style/base-editor.css';
